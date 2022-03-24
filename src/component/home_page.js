ffa893ed501f0fffed8aa8e6910d67486f2d42a8
suhaybcabdulahi <sabkacade40@gmail.com>
2022-03-24 18:03:57 +0530

    first commit

---
+++ src/component/home_page.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import CollectionPreview from './items'
+import SHOP_DATA from './shop.data.js';
+import { ShopPageContainer } from './itemstyle'
+
+class ShopPage extends React.Component {
+  constructor(props) {
+      super(props);
+      
+      this.state = {
+          collection: SHOP_DATA
+      }
+  }
+  render() {
+      // const {collections} = this.state;
+      
+      return(
+          <ShopPageContainer>
+              {
+                  this.state.collection.map(({id, ...otherCollection}) => (
+                      <CollectionPreview key={id} {...otherCollection} />
+                  ))
+              }
+          </ShopPageContainer>
+      )
+  }
+}
+
+export default ShopPage;
